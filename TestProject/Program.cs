64632b6e0bfed9d7c7bf4b428bbc8a74a3f0babe
Thi Vu <vudinhthi@gmail.com>
2020-03-08 15:29:03 +0700

    First commit

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -16,8 +16,9 @@ namespace TestProject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form3());
             //Application.Run(new Form2());
+            //Application.Run(new XtraReport1());
         }
     }
 }
--- TestProject/XtraReport1.cs
+++ TestProject/XtraReport1.cs
@@ -3,6 +3,7 @@ using System.Drawing;
 using System.Collections;
 using System.ComponentModel;
 using DevExpress.XtraReports.UI;
+using System.Drawing.Printing;
 
 namespace TestProject
 {
@@ -11,7 +12,42 @@ namespace TestProject
         public XtraReport1()
         {
             InitializeComponent();
+            CreateReport();
         }
+        public static XtraReport CreateReport()
+        {
 
+            // Create an XtraReport instance
+            XtraReport report = new XtraReport()
+            {
+                Name = "SimpleStaticReport",
+                DisplayName = "Simple Static Report",
+                PaperKind = PaperKind.Letter,
+                Margins = new Margins(100, 100, 100, 100)
+            };
+
+            // Create a Detail band for the report
+            DetailBand detailBand = new DetailBand()
+            {
+                HeightF = 25
+            };
+
+            // Add the created Detail band to the report
+            report.Bands.Add(detailBand);
+
+            // Create an XRLabel control for the report
+            XRLabel helloWordLabel = new XRLabel()
+            {
+                Text = "Hello, World!",
+                Font = new Font("Tahoma", 20f, FontStyle.Bold),
+                BoundsF = new RectangleF(0, 0, 250, 50),
+            };
+
+            // Add the created XRLabel to the Detail band
+            detailBand.Controls.Add(helloWordLabel);
+
+            // Return the report with a band and a label on it
+            return report;
+        }
     }
 }
